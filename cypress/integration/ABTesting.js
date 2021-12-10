806c1527ff55c86f8ed24f9396d09cd36986e74a
Praveen Pandey <praveenkumar.pandey@yara.com>
2021-12-10 14:52:05 +0530

    add a new feature file and minor changes to existing spec files

--- cypress/integration/ABTesting.js
+++ cypress/integration/ABTesting.js
@@ -1,12 +1,15 @@
 /// <reference types="Cypress" />
 
 beforeEach(() => {
-    cy.visit('/abtest');
-})
+  cy.visit("/abtest");
+});
 
-describe('A/B Testing', () => {
-    it('abc', () => {
-        cy.get('h3').should('contain', 'A/B Test');
-        cy.contains('p', 'Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text and/or functionality works best towards a desired outcome (e.g. a user action such as a click-through).').should('be.visible');
-    });
+describe("A/B Testing", () => {
+  it("abc", () => {
+    cy.get("h3").should("contain", "A/B Test");
+    cy.contains(
+      "p",
+      "Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text and/or functionality works best towards a desired outcome (e.g. a user action such as a click-through)."
+    ).should("be.visible");
+  });
 });
