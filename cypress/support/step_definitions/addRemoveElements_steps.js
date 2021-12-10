0a1e33872cc6c9df3e9e5970b179fe87c651261a
Praveen Pandey <praveenkumar.pandey@yara.com>
2021-12-10 18:58:28 +0530

    added new feature file and step definition

---
+++ cypress/support/step_definitions/addRemoveElements_steps.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+import { And, Then } from "cypress-cucumber-preprocessor/steps";
+
+Then('I verify that a button named {string} is displayed', (button_name) => {
+    cy.get('button').contains(button_name, {matchCase:false});
+});
+
+And('I click on {string} button', (button_name) => {
+    cy.get('button').contains(button_name, {matchCase:false}).click();
+});
