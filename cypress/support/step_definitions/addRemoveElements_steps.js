c6a2a85ca151efc428f4394e196ffd45314c0351
Praveen Pandey <praveenkumar.pandey@yara.com>
2021-12-19 17:35:53 +0530

    minor fix

--- cypress/support/step_definitions/addRemoveElements_steps.js
+++ cypress/support/step_definitions/addRemoveElements_steps.js
@@ -6,6 +6,6 @@ Then('I verify that a button named {string} is displayed', (button_name) => {
     cy.get('button').contains(button_name, {matchCase:false});
 });
 
-And(/^I click on {string} button$/, (button_name) => {
+And("I click on {string} button", (button_name) => {
     cy.get('button').contains(button_name, {matchCase:false}).click();
 });
